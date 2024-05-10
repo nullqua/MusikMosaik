2017230912611c61906567935679d28a61c50036
Florian <nullqua@mailbox.org>
2024-05-10 19:17:10 +0200

    Add files

---
+++ Playground/non-dragdrop-approach/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace non_dragdrop_approach
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
