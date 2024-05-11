2ec596e97b9037f870f900d1dc49b92222d95109
Florian <nullqua@mailbox.org>
2024-05-11 07:21:17 +0200

    Clean files

---
+++ Playground/DragDropExample/NodeConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Windows;
+
+namespace DragDropExample
+{
+    internal class NodeConnection
+    {
+        public UIElement StartItem { get; set; }
+        public UIElement EndItem { get; set; }
+        public Line ConnectionLine { get; set; }
+    }
+}
