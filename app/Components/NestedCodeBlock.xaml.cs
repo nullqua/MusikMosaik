3aaa1c2b1c25f503e89237cba8100f59086cf2ad
Florian <nullqua@mailbox.org>
2024-05-15 05:50:14 +0200

    Implement drag and drop of code blocks

---
+++ app/Components/NestedCodeBlock.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace app.Components
+{
+    /// <summary>
+    /// Interaction logic for NestedCodeBlock.xaml
+    /// </summary>
+    public partial class NestedCodeBlock : UserControl
+    {
+        public NestedCodeBlock()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty TypeProperty = DependencyProperty.Register(
+            "Type", typeof(string), typeof(NestedCodeBlock), new PropertyMetadata(default(string)));
+    
+        public static readonly DependencyProperty CountProperty = DependencyProperty.Register(
+            "Count", typeof(string), typeof(NestedCodeBlock), new PropertyMetadata(default(string)));
+
+        public static readonly DependencyProperty LoopCodeBlockProperty = DependencyProperty.Register(
+            "LoopCodeBlock", typeof(Border), typeof(NestedCodeBlock), new PropertyMetadata(default(Border)));
+
+        public Border LoopCodeBlock
+        {
+            get => (Border)GetValue(LoopCodeBlockProperty);
+            set => SetValue(LoopCodeBlockProperty, value);
+        }
+
+        public StackPanel CodeBlockContainer => container;
+
+        public string Type
+        {
+            get => (string)GetValue(TypeProperty);
+            set => SetValue(TypeProperty, value);
+        }
+
+        public string Count
+        {
+            get => (string)GetValue(CountProperty);
+            set => SetValue(CountProperty, value);
+        }
+
+        private void Container_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetData(typeof(Border)) == LoopCodeBlock)
+            {
+                MessageBox.Show("Geschachtelte Schleifen sind nicht erlaubt.", "Fehler");
+                e.Handled = true;
+                return;
+            }
+
+            if (e.Data.GetData(typeof(Border)) is Border codeBlock)
+            {
+                if (container.Children.Contains(placementHint))
+                {
+                    container.Children.Remove(placementHint);
+                }
+
+                var newCodeBlock = new Border
+                {
+                    Width = 70,
+                    Height = codeBlock.Height,
+                    Background = codeBlock.Background,
+                    Child = new TextBlock
+                    {
+                        Text = (codeBlock.Child as TextBlock)?.Text,
+                        FontSize = (codeBlock.Child as TextBlock)?.FontSize ?? 12,
+                        FontWeight = (codeBlock.Child as TextBlock)?.FontWeight ?? FontWeights.Normal,
+                        HorizontalAlignment = (codeBlock.Child as TextBlock)?.HorizontalAlignment ?? HorizontalAlignment.Left,
+                        VerticalAlignment = (codeBlock.Child as TextBlock)?.VerticalAlignment ?? VerticalAlignment.Top
+                    }
+                };
+
+                container.Children.Add(newCodeBlock);
+            }
+        }
+    }
+}
--- app/MainWindow.xaml.cs
+++ app/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using app.Components;
+using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +21,67 @@ namespace app
         public MainWindow()
         {
             InitializeComponent();
+
+            foreach (UIElement child in codeBlocksPanel.Children)
+            {
+                if (child is Border)
+                {
+                    child.PreviewMouseLeftButtonDown += CodeBlocksPanel_PreviewMouseLeftButtonDown;
+                }
+            }
+        }
+
+        private void CodeBlocksPanel_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            if (sender is Border codeBlock)
+            {
+                DragDrop.DoDragDrop(codeBlock, codeBlock, DragDropEffects.Copy);
+            }
+        }
+
+        private void CodeBlocksPlacement_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetData(typeof(Border)) is Border codeBlock)
+            {
+                if (codeBlocksPlacement.Children.Contains(placementHint))
+                {
+                    codeBlocksPlacement.Children.Remove(placementHint);
+                }
+
+                dynamic newCodeBlock;
+
+                if (codeBlock == loopCodeBlock)
+                {
+                    newCodeBlock = new NestedCodeBlock
+                    {
+                        Width = 140,
+                        Height = codeBlock.Height,
+                        Type = "Loop",
+                        Count = "5",
+                        LoopCodeBlock = loopCodeBlock
+                    };
+                }
+                else
+                {
+                    newCodeBlock = new Border
+                    {
+                        Width = 70,
+                        Height = codeBlock.Height,
+                        Background = codeBlock.Background,
+                        Child = new TextBlock
+                        {
+                            Text = (codeBlock.Child as TextBlock)?.Text,
+                            FontSize = (codeBlock.Child as TextBlock)?.FontSize ?? 12,
+                            FontWeight = (codeBlock.Child as TextBlock)?.FontWeight ?? FontWeights.Normal,
+                            HorizontalAlignment = (codeBlock.Child as TextBlock)?.HorizontalAlignment ?? HorizontalAlignment.Left,
+                            VerticalAlignment = (codeBlock.Child as TextBlock)?.VerticalAlignment ?? VerticalAlignment.Top
+                        }
+                    };
+                }
+                
+                codeBlocksPlacement.Children.Add(newCodeBlock);
+            }
         }
     }
 }
