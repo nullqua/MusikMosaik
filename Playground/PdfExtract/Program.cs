96a528106b1a34ce1696fffb6cac940cea3e0dca
Florian <nullqua@mailbox.org>
2024-05-20 11:50:34 +0200

    Initialize empty project

---
+++ Playground/PdfExtract/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PdfExtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
