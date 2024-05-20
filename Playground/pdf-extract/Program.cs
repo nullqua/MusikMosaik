85a3f5771234939c8fae3ae1687c67708d7afe5f
Florian <nullqua@mailbox.org>
2024-05-20 11:48:44 +0200

    Initialize project

---
+++ Playground/pdf-extract/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace pdf_extract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
