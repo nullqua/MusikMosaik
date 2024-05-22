5d0cc4674c251956a56dfd0a3c88498d115cc5a0
Florian <nullqua@mailbox.org>
2024-05-22 10:24:12 +0200

    Inititalize empty console project

---
+++ songfileGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace songfileGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
