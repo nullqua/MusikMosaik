be0c49a293aef11b8cb345e2724eef6cd199406d
Florian <nullqua@mailbox.org>
2024-06-23 11:27:54 +0200

    Update

--- app/CodeBlockOptionWindow.xaml.cs
+++ app/CodeBlockOptionWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using app.Model;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,34 @@ namespace app
         {
             InitializeComponent();
 
-            
+            var stackPanel = new StackPanel
+            {
+                Margin = new Thickness(10)
+            };
+
+            if (musicBlock is LoopBlock)
+            {
+                var label = new Label { Content = "Anzahl Durchläufe:" };
+                var textBox = new TextBox { Text = "1" };
+                stackPanel.Children.Add(label);
+                stackPanel.Children.Add(textBox);
+            }
+            else if (musicBlock is NoteBlock)
+            {
+                stackPanel.Children.Add(new Label { Content = "Tone:" });
+                var toneComboBox = new ComboBox();
+                toneComboBox.Items.Add("Low");
+                toneComboBox.Items.Add("Medium");
+                toneComboBox.Items.Add("High");
+                stackPanel.Children.Add(toneComboBox);
+
+                stackPanel.Children.Add(new Label { Content = "Note Length:" });
+                var noteLengthComboBox = new ComboBox();
+                noteLengthComboBox.Items.Add("Short");
+                noteLengthComboBox.Items.Add("Medium");
+                noteLengthComboBox.Items.Add("Long");
+                stackPanel.Children.Add(noteLengthComboBox);
+            }
         }
     }
 }
