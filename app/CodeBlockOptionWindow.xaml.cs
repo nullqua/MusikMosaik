94fda8fd88dee8caeee826d7fd8f0d9e72737ffb
Florian <nullqua@mailbox.org>
2024-06-27 16:35:08 +0200

    Implement Button event handling

--- app/CodeBlockOptionWindow.xaml.cs
+++ app/CodeBlockOptionWindow.xaml.cs
@@ -22,9 +22,20 @@ namespace app
     /// </summary>
     public partial class CodeBlockOptionWindow : Window
     {
+        private readonly MusicBlock musicBlock;
+
+        private readonly TextBox loopCount;
+        private readonly ComboBox tone1;
+        private readonly ComboBox tone2;
+        private readonly ComboBox pitch;
+        private readonly ComboBox noteLength;
+        private readonly ComboBox scale;
+
         public CodeBlockOptionWindow(ref MusicBlock musicBlock)
         {
             InitializeComponent();
+
+            this.musicBlock = musicBlock;
 
             var stackPanel = new StackPanel
             {
@@ -34,137 +45,137 @@ namespace app
             if (musicBlock is LoopBlock)
             {
                 var label = new Label { Content = "Anzahl Durchläufe:" };
-                var textBox = new TextBox { Text = "1" };
+                loopCount = new TextBox { Text = "1" };
 
-                textBox.PreviewTextInput += TextBox_PreviewTextInput;
+                loopCount.PreviewTextInput += TextBox_PreviewTextInput;
                 stackPanel.Children.Add(label);
-                stackPanel.Children.Add(textBox);
+                stackPanel.Children.Add(loopCount);
             }
             else if (musicBlock is NoteBlock)
             {
                 stackPanel.Children.Add(new Label { Content = "Ton:" });
 
-                var noteComboBox = new ComboBox();
+                tone1 = new ComboBox();
 
-                noteComboBox.Items.Add("H#/C");
-                noteComboBox.Items.Add("C#/Db");
-                noteComboBox.Items.Add("D");
-                noteComboBox.Items.Add("D#/Eb");
-                noteComboBox.Items.Add("E/Fb");
-                noteComboBox.Items.Add("E#/F");
-                noteComboBox.Items.Add("F#/Gb");
-                noteComboBox.Items.Add("G");
-                noteComboBox.Items.Add("G#/Ab");
-                noteComboBox.Items.Add("A");
-                noteComboBox.Items.Add("A#/B");
-                noteComboBox.Items.Add("H/Cb");
+                tone1.Items.Add("H#/C");
+                tone1.Items.Add("C#/Db");
+                tone1.Items.Add("D");
+                tone1.Items.Add("D#/Eb");
+                tone1.Items.Add("E/Fb");
+                tone1.Items.Add("E#/F");
+                tone1.Items.Add("F#/Gb");
+                tone1.Items.Add("G");
+                tone1.Items.Add("G#/Ab");
+                tone1.Items.Add("A");
+                tone1.Items.Add("A#/B");
+                tone1.Items.Add("H/Cb");
 
-                stackPanel.Children.Add(noteComboBox);
+                stackPanel.Children.Add(tone1);
 
                 stackPanel.Children.Add(new Label { Content = "Tonhöhe:" });
 
-                var pitchComboBox = new ComboBox();
+                pitch = new ComboBox();
 
-                pitchComboBox.Items.Add("1 (tiefste Oktave)");
-                pitchComboBox.Items.Add("2");
-                pitchComboBox.Items.Add("3");
-                pitchComboBox.Items.Add("4 (Mitte der Klaviatur)");
-                pitchComboBox.Items.Add("5");
-                pitchComboBox.Items.Add("6");
-                pitchComboBox.Items.Add("7");
-                pitchComboBox.Items.Add("8 (höchste Oktave)");
+                pitch.Items.Add("1 (tiefste Oktave)");
+                pitch.Items.Add("2");
+                pitch.Items.Add("3");
+                pitch.Items.Add("4 (Mitte der Klaviatur)");
+                pitch.Items.Add("5");
+                pitch.Items.Add("6");
+                pitch.Items.Add("7");
+                pitch.Items.Add("8 (höchste Oktave)");
 
-                stackPanel.Children.Add(pitchComboBox);
+                stackPanel.Children.Add(pitch);
 
                 stackPanel.Children.Add(new Label { Content = "Tonlänge:" });
 
-                var noteLengthComboBox = new ComboBox();
-                noteLengthComboBox.Items.Add("ganze Note");
-                noteLengthComboBox.Items.Add("halbe Note");
-                noteLengthComboBox.Items.Add("viertel Note");
-                noteLengthComboBox.Items.Add("punktierte viertel Note");
-                noteLengthComboBox.Items.Add("achtel Note");
-                noteLengthComboBox.Items.Add("punktierte achtel Note");
-                noteLengthComboBox.Items.Add("16tel Note");
+                noteLength = new ComboBox();
+                noteLength.Items.Add("ganze Note");
+                noteLength.Items.Add("halbe Note");
+                noteLength.Items.Add("viertel Note");
+                noteLength.Items.Add("punktierte viertel Note");
+                noteLength.Items.Add("achtel Note");
+                noteLength.Items.Add("punktierte achtel Note");
+                noteLength.Items.Add("16tel Note");
 
-                stackPanel.Children.Add(noteLengthComboBox);
+                stackPanel.Children.Add(noteLength);
             }
             else if (musicBlock is ChordBlock)
             {
                 stackPanel.Children.Add(new Label { Content = "Tonlänge:" });
 
-                var noteLengthComboBox = new ComboBox();
-                noteLengthComboBox.Items.Add("ganze Note");
-                noteLengthComboBox.Items.Add("halbe Note");
-                noteLengthComboBox.Items.Add("viertel Note");
-                noteLengthComboBox.Items.Add("punktierte viertel Note");
-                noteLengthComboBox.Items.Add("achtel Note");
-                noteLengthComboBox.Items.Add("punktierte achtel Note");
-                noteLengthComboBox.Items.Add("16tel Note");
+                noteLength = new ComboBox();
+                noteLength.Items.Add("ganze Note");
+                noteLength.Items.Add("halbe Note");
+                noteLength.Items.Add("viertel Note");
+                noteLength.Items.Add("punktierte viertel Note");
+                noteLength.Items.Add("achtel Note");
+                noteLength.Items.Add("punktierte achtel Note");
+                noteLength.Items.Add("16tel Note");
 
-                stackPanel.Children.Add(noteLengthComboBox);
+                stackPanel.Children.Add(noteLength);
 
                 stackPanel.Children.Add(new Label { Content = "Grundton:" });
 
-                var baseTone = new ComboBox();
+                tone1 = new ComboBox();
 
-                baseTone.Items.Add("H#/C");
-                baseTone.Items.Add("C#/Db");
-                baseTone.Items.Add("D");
-                baseTone.Items.Add("D#/Eb");
-                baseTone.Items.Add("E/Fb");
-                baseTone.Items.Add("E#/F");
-                baseTone.Items.Add("F#/Gb");
-                baseTone.Items.Add("G");
-                baseTone.Items.Add("G#/Ab");
-                baseTone.Items.Add("A");
-                baseTone.Items.Add("A#/B");
-                baseTone.Items.Add("H/Cb");
+                tone1.Items.Add("H#/C");
+                tone1.Items.Add("C#/Db");
+                tone1.Items.Add("D");
+                tone1.Items.Add("D#/Eb");
+                tone1.Items.Add("E/Fb");
+                tone1.Items.Add("E#/F");
+                tone1.Items.Add("F#/Gb");
+                tone1.Items.Add("G");
+                tone1.Items.Add("G#/Ab");
+                tone1.Items.Add("A");
+                tone1.Items.Add("A#/B");
+                tone1.Items.Add("H/Cb");
 
-                stackPanel.Children.Add(baseTone);
+                stackPanel.Children.Add(tone1);
 
                 stackPanel.Children.Add(new Label { Content = "Höchster Ton:" });
 
-                var overTone = new ComboBox();
+                tone2 = new ComboBox();
 
-                overTone.Items.Add("H#/C");
-                overTone.Items.Add("C#/Db");
-                overTone.Items.Add("D");
-                overTone.Items.Add("D#/Eb");
-                overTone.Items.Add("E/Fb");
-                overTone.Items.Add("E#/F");
-                overTone.Items.Add("F#/Gb");
-                overTone.Items.Add("G");
-                overTone.Items.Add("G#/Ab");
-                overTone.Items.Add("A");
-                overTone.Items.Add("A#/B");
-                overTone.Items.Add("H/Cb");
+                tone2.Items.Add("H#/C");
+                tone2.Items.Add("C#/Db");
+                tone2.Items.Add("D");
+                tone2.Items.Add("D#/Eb");
+                tone2.Items.Add("E/Fb");
+                tone2.Items.Add("E#/F");
+                tone2.Items.Add("F#/Gb");
+                tone2.Items.Add("G");
+                tone2.Items.Add("G#/Ab");
+                tone2.Items.Add("A");
+                tone2.Items.Add("A#/B");
+                tone2.Items.Add("H/Cb");
 
-                stackPanel.Children.Add(overTone);
+                stackPanel.Children.Add(tone2);
 
                 stackPanel.Children.Add(new Label { Content = "Oktave (höchste Note):" });
 
-                var pitchComboBox = new ComboBox();
+                pitch = new ComboBox();
 
-                pitchComboBox.Items.Add("1 (tiefste Oktave)");
-                pitchComboBox.Items.Add("2");
-                pitchComboBox.Items.Add("3");
-                pitchComboBox.Items.Add("4 (Mitte der Klaviatur)");
-                pitchComboBox.Items.Add("5");
-                pitchComboBox.Items.Add("6");
-                pitchComboBox.Items.Add("7");
-                pitchComboBox.Items.Add("8 (höchste Oktave)");
+                pitch.Items.Add("1 (tiefste Oktave)");
+                pitch.Items.Add("2");
+                pitch.Items.Add("3");
+                pitch.Items.Add("4 (Mitte der Klaviatur)");
+                pitch.Items.Add("5");
+                pitch.Items.Add("6");
+                pitch.Items.Add("7");
+                pitch.Items.Add("8 (höchste Oktave)");
 
-                stackPanel.Children.Add(pitchComboBox);
+                stackPanel.Children.Add(pitch);
 
                 stackPanel.Children.Add(new Label { Content = "Akkord-Art:" });
 
-                var scaleComboBox = new ComboBox();
+                scale = new ComboBox();
 
-                scaleComboBox.Items.Add("Dur");
-                scaleComboBox.Items.Add("Moll");
+                scale.Items.Add("Dur");
+                scale.Items.Add("Moll");
 
-                stackPanel.Children.Add(scaleComboBox);
+                stackPanel.Children.Add(scale);
             }
 
             panel.Children.Add(stackPanel);
@@ -174,6 +185,25 @@ namespace app
         {
             var textBox = sender as TextBox;
             e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (musicBlock is LoopBlock loopBlock)
+            {
+                if (loopCount.Text != "")
+                {
+                    loopBlock.RepeatCount = int.Parse(loopCount.Text);
+                }
+            }
+            else if (musicBlock is NoteBlock noteBlock)
+            {
+
+            }
+            else if (musicBlock is ChordBlock chordBlock)
+            {
+
+            }
         }
     }
 }
